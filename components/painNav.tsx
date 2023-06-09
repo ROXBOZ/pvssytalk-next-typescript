45082fe935a1278c10bb8d154c7f01c935615b8c
ROXBOZ <hello@roxanne-borloz.net>
2023-06-09 22:56:53 +0200

    built article

--- components/painNav.tsx
+++ components/painNav.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { ourUrl } from "../utils/urls";
 
 interface PainNavProps {
   pain: {
     name: string;
   };
+  isMed: boolean;
+  setIsMed: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const PainNav = ({ pain }: PainNavProps) => {
+const PainNav = ({ pain, isMed, setIsMed }: PainNavProps) => {
   let currentURL;
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
   if (typeof window !== "undefined") {
     currentURL = window.location.pathname;
     console.log("currentURL:", currentURL);
@@ -16,20 +20,46 @@ const PainNav = ({ pain }: PainNavProps) => {
     console.log("Window object is not available.");
   }
 
+  const switchArticle = () => {
+    setIsMed((prevIsMed) => !prevIsMed);
+  };
+
+  const copyUrlToClipboard = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
+  };
+
   return (
     <nav className="pain-nav">
       <h3>
-        Ressources <span className="colored nowrap">{pain.name}</span>
+        Approche{" "}
+        <span className="colored">
+          {isMed ? <span>médicale</span> : <span>sexologique</span>}
+        </span>
       </h3>
-      <a href="/glossaire">Glossaire</a>
-      <a href="/exercices">Exercices</a>
-      <a href="/medias">Littérature et médias</a>
-      <h4>Partager</h4>
+      <button onClick={switchArticle}>
+        Voir l’approche{" "}
+        {isMed ? <span>sexologique</span> : <span>médicale</span>}
+      </button>
+
+      <h3>Ressources</h3>
+      <a href="glossaire">Glossaire</a>
+      <a href="exercices">Exercices</a>
+      <a href="medias">Littérature et médias</a>
+      <h3>Partager</h3>
       <a href={`whatsapp://send?text=${ourUrl}${currentURL}`}>WhatsApp</a>
       <a href={`https://telegram.me/share/url?url=${ourUrl}${currentURL}`}>
         Telegram
       </a>
-      <a href="mailto:hello@pvssy-talk.org">Email</a>
+      <a href="mailto:?subject=Un%20lien%20qui%20pourrait%20t%E2%80%99int%C3%A9resser%20sur%20pvssy-talk.org&body=%C3%87a%20devrait%20t%E2%80%99int%C3%A9resser%20%3A%20https%3A%2F%2Fwww.pvssy-talk.org%2Fdouleurs%2Fvaginisme">
+        Email
+      </a>
+      <a style={{ cursor: "pointer" }} onClick={copyUrlToClipboard}>
+        Copier l’URL
+      </a>
     </nav>
   );
 };
--- pages/douleurs/[pain]/approche-medicale.tsx
+++ pages/douleurs/[pain]/approche-medicale.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { PainDetail } from "../../../types";
 import { client, urlFor } from "../../../utils/sanity/client";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import { PortableText } from "@portabletext/react";
 import PainNav from "../../../components/painNav";
+import { Link } from "react-router-dom";
 
 interface Schema {
   schemaImage: {
@@ -21,6 +22,7 @@ const approcheMedicale = ({ pain }: { pain: PainDetail }) => {
   const imageCoverHotspot = {
     objectPosition: `${imgHotspot?.x * 100}% ${imgHotspot?.y * 100}%`,
   };
+  const [isMed, setIsMed] = useState<boolean>(true);
 
   return (
     <main>
@@ -38,62 +40,148 @@ const approcheMedicale = ({ pain }: { pain: PainDetail }) => {
       )}
 
       <div className="pain-nav-article-container">
-        <PainNav pain={pain} />
+        <PainNav pain={pain} isMed={isMed} setIsMed={setIsMed} />
 
         <div className="pain-article">
-          {pain.medicalApproach?.def && (
+          {isMed ? (
             <>
-              <h2>Définition</h2>
-              <PortableText value={pain.medicalApproach.def} />
+              {pain.medicalApproach?.def && (
+                <>
+                  <h2>Définition</h2>
+                  <PortableText value={pain.medicalApproach.def} />
+                </>
+              )}
+              {pain.medicalApproach?.schemas && (
+                <>
+                  <h3>En images</h3>
+                  <div className="schemas-container">
+                    {Object.keys(pain.medicalApproach.schemas).map(
+                      (key: string, index: number) => {
+                        const schema = pain.medicalApproach.schemas[
+                          key
+                        ] as Schema;
+                        return (
+                          <div key={index}>
+                            <Image
+                              className="schema"
+                              src={urlFor(schema.schemaImage.asset._ref).url()}
+                              width={2000}
+                              height={400}
+                              alt={schema.schemaImage.alternativeText}
+                            />
+                            <p className="schema-caption">
+                              {schema.schemaImage.caption}
+                            </p>
+                          </div>
+                        );
+                      }
+                    )}
+                  </div>
+                </>
+              )}
+              {pain.medicalApproach?.diag && (
+                <>
+                  <h2>Diagnostic</h2>
+                  <PortableText value={pain.medicalApproach.diag} />
+                </>
+              )}
+              {pain.medicalApproach?.sympt && (
+                <>
+                  <h2>Symptômes</h2>
+                  <PortableText value={pain.medicalApproach.sympt} />
+                </>
+              )}
+              {pain.medicalApproach?.why && (
+                <>
+                  <h2>Pourquoi ça m’arrive ?</h2>
+                  <PortableText value={pain.medicalApproach.why} />
+                </>
+              )}
+              {pain.medicalApproach?.auto && (
+                <>
+                  <h2>Que puis-je faire solo ?</h2>
+                  <PortableText value={pain.medicalApproach.auto} />
+                </>
+              )}
+              {pain.medicalApproach?.pros && (
+                <>
+                  <h2>Qui consulter ?</h2>
+                  <PortableText value={pain.medicalApproach.pros} />
+                </>
+              )}
+            </>
+          ) : (
+            <>
+              <h2>Vivre avec la douleur</h2>
+              {pain.sexologicApproach?.body && (
+                <>
+                  <h3>Moi et mon corps</h3>
+                  <PortableText value={pain.sexologicApproach.body} />
+                </>
+              )}
+              {pain.sexologicApproach?.norms && (
+                <>
+                  <h3>Normes genrées</h3>
+                  <PortableText value={pain.sexologicApproach.norms} />
+                </>
+              )}
+              {pain.sexologicApproach?.everydayLife && (
+                <>
+                  <h3>Vie quotidienne</h3>
+                  <PortableText value={pain.sexologicApproach.everydayLife} />
+                </>
+              )}
+              <h2>Sexualité avec la douleur</h2>
+              {pain.sexologicApproach?.libido && (
+                <>
+                  <h3>Libido</h3>
+                  <PortableText value={pain.sexologicApproach.libido} />
+                </>
+              )}
+              {pain.sexologicApproach?.charge && (
+                <>
+                  <h3>Charge mentale et communication</h3>
+                  <PortableText value={pain.sexologicApproach.charge} />
+                </>
+              )}
+              {pain.sexologicApproach?.consent && (
+                <>
+                  <h3>Sexe et consentement</h3>
+                  <PortableText value={pain.sexologicApproach.consent} />
+                </>
+              )}
+              {pain.sexologicApproach?.mental && (
+                <>
+                  <h2>Santé mentale</h2>
+                  <PortableText value={pain.sexologicApproach.mental} />
+                </>
+              )}
+              {pain.sexologicApproach?.parenthood && (
+                <>
+                  <h2>Parentalité</h2>
+                  <PortableText value={pain.sexologicApproach.parenthood} />
+                </>
+              )}
+              {pain.sexologicApproach?.checkup && (
+                <>
+                  <h2>Avec les pros de la santé</h2>
+                  <PortableText value={pain.sexologicApproach.checkup} />
+                </>
+              )}
+              {pain.sexologicApproach?.treatments && (
+                <>
+                  <h2>Quels traitements pour me soulager ?</h2>
+                  <PortableText value={pain.sexologicApproach.treatments} />
+                </>
+              )}
+              {pain.sexologicApproach?.pleasure && (
+                <>
+                  <h2>Plaisir/ anti-douleur</h2>
+                  <PortableText value={pain.sexologicApproach.pleasure} />
+                </>
+              )}
             </>
           )}
-
-          {pain.medicalApproach?.schemas && (
-            <>
-              <h3>En images</h3>
-              <div className="schemas-container">
-                {Object.keys(pain.medicalApproach.schemas).map(
-                  (key: string, index: number) => {
-                    const schema = pain.medicalApproach.schemas[key] as Schema;
-                    return (
-                      <div key={index}>
-                        <Image
-                          className="schema"
-                          src={urlFor(schema.schemaImage.asset._ref).url()}
-                          width={2000}
-                          height={400}
-                          alt={schema.schemaImage.alternativeText}
-                        />
-                        <p className="schema-caption">
-                          {schema.schemaImage.caption}
-                        </p>
-                      </div>
-                    );
-                  }
-                )}
-              </div>
-            </>
-          )}
-
-          {pain.medicalApproach?.diag && (
-            <>
-              <h2>Diagnostic</h2>
-              <PortableText value={pain.medicalApproach.diag} />
-            </>
-          )}
-          {pain.medicalApproach?.sympt && (
-            <>
-              <h2>Symptômes</h2>
-              <PortableText value={pain.medicalApproach.sympt} />
-            </>
-          )}
-          {pain.medicalApproach?.why && (
-            <>
-              <h2>Pourquoi ça m’arrive ?</h2>
-              <PortableText value={pain.medicalApproach.why} />
-            </>
-          )}
-          <p style={{ color: "red" }}>... suite</p>
         </div>
       </div>
     </main>
