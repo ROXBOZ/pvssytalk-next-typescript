83bcc0a3e63fd9d331fbdea8a273d74cccce9e85
Roxanne Borloz <hello@roxanne-borloz.net>
2024-03-18 12:02:37 +0100

    remove page transition

--- components/layouts/PageTransition.tsx
+++ components/layouts/PageTransition.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 function PageTransition({ children }: any) {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{
-        opacity: 0,
-        translateY: -200,
-      }}
-      transition={{ duration: 0.25 }}
-    >
-      {children}
-    </motion.div>
+    // <motion.div
+    //   initial={{ opacity: 0 }}
+    //   animate={{ opacity: 1 }}
+    //   exit={{
+    //     opacity: 0,
+    //     translateY: -200,
+    //   }}
+    //   transition={{ duration: 0.25 }}
+    // >
+    <> {children}</>
+    // </motion.div>
   );
 }
 
