9ed061eae743719a8fd5f6d3670c8845c41983d2
Roxanne Borloz <hello@roxanne-borloz.net>
2024-03-18 11:55:49 +0100

    viewport fix

--- components/CustomHead.tsx
+++ components/CustomHead.tsx
@@ -4,10 +4,16 @@ import React from "react";
 function CustomHead({ seo }: any) {
   return (
     <Head>
-      {seo && seo.pageTitle && <title>{seo.pageTitle}</title>}
-      {seo && seo.metaDescription && (
-        <meta name="description" content={seo.metaDescription} />
-      )}
+      <>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        {seo && seo.pageTitle && <title>{seo.pageTitle}</title>}
+        {seo && seo.metaDescription && (
+          <meta name="description" content={seo.metaDescription} />
+        )}
+      </>
     </Head>
   );
 }
