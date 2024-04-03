e97695bd256eff8c267205e4e2804a79b687ccc3
Roxanne Borloz <hello@roxanne-borloz.net>
2024-04-03 14:49:18 +0200

    tagline

--- components/Tagline.tsx
+++ components/Tagline.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const Tagline = ({ tagline }: any) => {
   return (
     <div className="tagline">
-      <h1>{tagline}</h1>
+      <div className="gradient" />
+      <div className="content">
+        <h1>{tagline}</h1>
+      </div>
     </div>
   );
 };
