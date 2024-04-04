8531d56c47038db490c2b7aff985391d65ea4a2c
Roxanne Borloz <hello@roxanne-borloz.net>
2024-04-04 17:12:18 +0200

    all

--- components/DotsZone.tsx
+++ components/DotsZone.tsx
@@ -1,12 +1,42 @@
+import React, { useEffect } from "react";
+
 import Link from "next/link";
-import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useInView } from "react-intersection-observer";
 
 function DotsZone(data: any) {
   const text = data.data.text;
   const label = data.data.callToAction.label;
   const url = data.data.callToAction.link.slug.current;
+
+  const { ref, inView } = useInView({
+    threshold: 0.5, // Trigger animation when 50% of the component is in view
+    triggerOnce: true, // Trigger animation only once
+  });
+
+  useEffect(() => {
+    if (inView) {
+      gsap.fromTo(
+        ".dot",
+        { opacity: 0, scale: 0 },
+        {
+          opacity: 1,
+          scale: 1,
+          duration: 0.5,
+
+          stagger: {
+            each: 0.1,
+            from: "edges",
+          },
+          ease: "power2.inOut",
+        }
+      );
+    }
+  }, [inView]);
+
   return (
-    <div className="dots-zone snap-section">
+    <div className="dots-zone snap-section" ref={ref}>
       <div className="content">
         <p className="h3">{text}</p>
         <Link href={`/${url}`} style={{ borderBottom: 0 }}>
@@ -14,31 +44,31 @@ function DotsZone(data: any) {
         </Link>
       </div>
       <div className="dots-container">
-        <div className="primary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="primary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="primary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="primary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="primary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
-        <div className="secondary dot" />
+        <div className="dot primary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot primary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot primary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot primary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot primary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
+        <div className="dot secondary" />
       </div>
     </div>
   );
