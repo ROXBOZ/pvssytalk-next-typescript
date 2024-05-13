ed09a67b53fad07e8f247beaa3290801ec7bc007
Roxanne Borloz <hello@roxanne-borloz.net>
2024-05-13 14:34:33 +0200

    matomo

--- components/CustomHead.tsx
+++ components/CustomHead.tsx
@@ -8,6 +8,24 @@ function CustomHead({ seo }: any) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="https://pvssytalk.matomo.cloud/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src='https://cdn.matomo.cloud/pvssytalk.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `,
+          }}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
               window.biskouiSettings = {
                 clientId: "4568013",
               };
